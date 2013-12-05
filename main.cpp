a37ea08fefcdb841b86b85086194f5aea60ccc0d
Alexander Bean Apmann <caboose200@gmail.com>
2013-12-05 18:05:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/*
+Author: Alexander Bean Apmann
+Class: Dean Lawson's
+Assignment: Final
+Date Assigned: November
+Due Date: December
+Description: Snaaaaaaaaaaake
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+*/
+
 #include "game.h"
 
 int main()
